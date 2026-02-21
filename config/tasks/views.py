10947e2fb9a7cbf288e04fb5601387faa70b1e1d
Neeraj Bhoir <neerajkb23@gmail.com>
2026-02-21 11:18:06 +0530

    Added user-task relationship, implemented login protection, and filtered tasks per authenticated user

--- config/tasks/views.py
+++ config/tasks/views.py
@@ -1,19 +1,24 @@
 from django.shortcuts import render, redirect
 from .models import Task
+from django.contrib.auth.decorators import login_required
 
-# list all tasks
+
+# show tasks
+@login_required
 def tasks_home(request):
-    tasks = Task.objects.all()
+    tasks = Task.objects.filter(user=request.user)
     return render(request, 'tasks/task_list.html', {'tasks': tasks})
 
 
-# create a new task
+# create task
+@login_required
 def add_task(request):
     if request.method == 'POST':
         title = request.POST.get('title')
         description = request.POST.get('description')
 
         Task.objects.create(
+            user=request.user,
             title=title,
             description=description,
             completed=False
